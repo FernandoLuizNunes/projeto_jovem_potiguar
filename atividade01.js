d414400accc2a1bc746a978efb55f3d42d919ee7
Fernando Luiz Nunes <fernandoluiiznunes@gmail.com>
2024-12-12 21:10:13 -0300

    Fernando

---
+++ atividade01.js
@@ -0,0 +1,8 @@
+//Declaro uma variável opara armaenar o nome de um aluno, sua idade, e
+// sua nota final. Exiba valores no console. 
+
+let nome = "Fernando Luiz"
+let idade = 25
+let notaFinal = 9.5
+
+console.log("Nome: ", nome, "Idade: ", idade, "Nota Final: ", notaFinal)
